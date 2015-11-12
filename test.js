9b12368f8a963dace06d745593719258b6259f7b
Simen Li <simenkid@gmail.com>
2015-11-12 18:27:04 +0800

    going

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
-var x = {
-	x1: 3,
-	x2: (function () {
-		return this.x1;
-	}())
-};
-
+var x = '/xxxxy/';
 console.log(x);
+// x = x.replace('.', function (c) {
+// 	return '/';
+// });
+x = x.replace(/\./g, '/');
+// for (var i = 0, len = x.length; i < len; i++) {
+// 	if (x[i] === ".") {
+// 		console.log('fuck');
+// 		x[i] = '0';
+// 	}//x[i] = "a";
+// 	//console.log(x[i]);
+// }
+console.log(x);
+// if (x[0] === '/') x = x.slice(1);
+// if (x[x.length-1] === '/') {
+// 	x = x.slice(0, x.length-1);
+// 	console.log(x);
+// }
+
+// var y = x.split('/');
+// console.log(y);
