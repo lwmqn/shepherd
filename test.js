702dbe9e380c4e1175b9bf4ed18f96a9d7370e2c
Simen Li <simenkid@gmail.com>
2015-11-16 18:39:39 +0800

    going

--- test.js
+++ test.js
@@ -1,4 +1,41 @@
-var t = setTimeout(function () {}, 5000);
-var t1 = setInterval(function () {}, 5000);
-console.log(typeof t);
-console.log(typeof t1);
+var _ = require('lodash');
+
+var x = {
+    'barney': {
+        kelly: {
+            john: 'doe',
+            '1': [ 220, 110 ]
+        },
+        yoman: 'pppp'
+    },
+    'fred': 40,
+    hey: {
+        man: 'here'
+    }
+};
+
+var res = extract('', x);
+
+console.log(res);
+
+function extract(rootPath, obj, arr) {
+    var result = {};
+
+    if (rootPath !== '' && rootPath !== '.' && rootPath !== '/' && !_.isUndefined(rootPath))
+        rootPath = rootPath + '.';
+
+    _.forEach(obj, function (n, key) {
+        if (_.isObject(n)) {
+            var tmp;
+            if (arr)
+                key = '[' + key + ']';
+
+            tmp = extract(rootPath + key, n, _.isArray(n));
+            _.assign(result, tmp);
+        } else {
+            result[rootPath + key] = n;
+        }
+    });
+
+    return result;
+}
