80cf3ce2860c10fd7e8cde046a75bf89a228bdd6
Simen Li <simenkid@gmail.com>
2015-11-25 18:33:02 +0800

    debugging

---
+++ sever.js
@@ -0,0 +1,12 @@
+var Shepherd = require('./index');
+
+var shepherd = new Shepherd('my_shepherd');
+shepherd.start();
+shepherd.on('ready', function () {
+	console.log('shepherd ready');
+	//console.log(shepherd);
+});
+
+shepherd.on('error', function (err) {
+	console.log(err);
+});
