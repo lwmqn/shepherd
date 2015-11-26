a3056a1c1d76cc35e7f38e9f2ca3bb02c1a0394c
Simen Li <simenkid@gmail.com>
2015-11-26 16:46:34 +0800

    under testing

--- sever.js
+++ sever.js
@@ -3,10 +3,10 @@ var Shepherd = require('./index');
 var shepherd = new Shepherd('my_shepherd');
 shepherd.start();
 shepherd.on('ready', function () {
-	console.log('shepherd ready');
-	//console.log(shepherd);
+    console.log('shepherd ready');
+    //console.log(shepherd);
 });
 
 shepherd.on('error', function (err) {
-	console.log(err);
+    console.log(err);
 });
