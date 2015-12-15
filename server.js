38b0cbd9df089f6d7df440c03c0ffa47397233b8
Simen Li <simenkid@gmail.com>
2015-12-15 17:23:01 +0800

    notify and request are waiting for testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var shepherd = new Shepherd('my_shepherd');
 
 
 shepherd.start(function (err, res) {
-	if (err) console.log(err);
+    if (err) console.log(err);
 });
 
 shepherd.on('ready', function () {
@@ -12,17 +12,21 @@ shepherd.on('ready', function () {
     //console.log(shepherd);
 });
 
+shepherd.on('updated', function (diff) {
+    console.log(diff);
+});
+
 shepherd.on('error', function (err) {
     console.log(err);
 });
 
 shepherd.on('notified', function (diff) {
-	console.log('%%%%%%%%%%%%%% NOTIFIED %%%%%%%%%%%%%%%%%%%%');
-    console.log(diff);
+    // console.log('%%%%%%%%%%%%%% NOTIFIED %%%%%%%%%%%%%%%%%%%%');
+    // console.log(diff);
 });
 
 shepherd.on('registered', function (node) {
-	console.log('REGISTERED');
+    console.log('REGISTERED');
     console.log(node.clientId);
     console.log(node.status);
 
@@ -38,7 +42,9 @@ shepherd.on('registered', function (node) {
     //     step: null,               // only valid for number
     //     // lastReportedValue: xxx // optional
     // };
-
+// console.log(node.shepherd._rspsToResolve.mnode_1.read[0]);
+// console.log(node.shepherd._rspsToResolve.mnode_1.read[1]);
+// console.log(node.shepherd._rspsToResolve.mnode_1.read[2]);
     setTimeout(function () {
 //     setInterval(function () {
             node.writeAttrsReq('/device/0/manuf', {
@@ -50,42 +56,55 @@ shepherd.on('registered', function (node) {
             }).done(function (r) {
              console.log('>>>>>>>> Write Attrs');
              console.log(r);
+                setTimeout(function () {
+                    shepherd.announce('Announce to the world!!!');
+                }, 3000);
+
+                setTimeout(function () {
+                    console.log(node.shepherd._rspsToResolve.mnode_1);
+                }, 16000);
             }, function (err) {
-             console.log(err);
+                console.log('>>>>>>>> Write Attrs ERR');
+                console.log(err);
+                console.log(node.shepherd._nodebox[node.clientId]);
+                setTimeout(function () {
+                    console.log(node.shepherd._rspsToResolve);
+                }, 3000);
+                
             });
      // }, 3200);
 
     }, 5000);
 
-    node.readReq('tempSensor/').done(function (r) {
-    	console.log('>>>>>>>> read tempSensor Object');
-    	console.log(r);
-    }, function (err) {
-    	console.log(err);
-    });
+    // node.readReq('tempSensor/').done(function (r) {
+    //     console.log('>>>>>>>> read tempSensor Object');
+    //     console.log(r);
+    // }, function (err) {
+    //     console.log(err);
+    // });
 
-    setTimeout(function () {
-    	setInterval(function () {
-		    node.readReq('tempSensor/0/').done(function (r) {
-		    	console.log('>>>>>>>> read tempSensor Instance');
-		    	console.log(r);
-		    }, function (err) {
-		    	console.log(err);
-		    });
-    	}, 3200);
+    // setTimeout(function () {
+    //     setInterval(function () {
+    //         node.readReq('tempSensor/0/').done(function (r) {
+    //             console.log('>>>>>>>> read tempSensor Instance');
+    //             console.log(r);
+    //         }, function (err) {
+    //             console.log(err);
+    //         });
+    //     }, 3200);
 
-    }, 12000);
+    // }, 12000);
 
-    setTimeout(function () {
-    	setInterval(function () {
-		    node.readReq('tempSensor/0/sensorValue').done(function (r) {
-		    	console.log('>>>>>>>> read tempSensor Resource');
-		    	console.log(r);
-		    }, function (err) {
-		    	console.log(err);
-		    });
-    	}, 4000);
+    // setTimeout(function () {
+    //     setInterval(function () {
+    //         node.readReq('tempSensor/0/sensorValue').done(function (r) {
+    //             console.log('>>>>>>>> read tempSensor Resource');
+    //             console.log(r);
+    //         }, function (err) {
+    //             console.log(err);
+    //         });
+    //     }, 4000);
 
-    }, 15000);
+    // }, 15000);
 
 });
