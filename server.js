4076eaddd547fb72ded04f8b9f99303296b96ba1
Simen Li <simenkid@gmail.com>
2015-12-16 18:44:46 +0800

    last 2 APIs to test: writeReq and observeReq

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var Shepherd = require('./index');
+var util = require('util'),
+    Shepherd = require('./index');
 
 var shepherd = new Shepherd('my_shepherd');
 
@@ -21,8 +22,8 @@ shepherd.on('error', function (err) {
 });
 
 shepherd.on('notified', function (diff) {
-    // console.log('%%%%%%%%%%%%%% NOTIFIED %%%%%%%%%%%%%%%%%%%%');
-    // console.log(diff);
+    console.log('%%%%%%%%%%%%%% NOTIFIED %%%%%%%%%%%%%%%%%%%%');
+    console.log(diff);
 });
 
 shepherd.on('registered', function (node) {
@@ -42,18 +43,19 @@ shepherd.on('registered', function (node) {
     //     step: null,               // only valid for number
     //     // lastReportedValue: xxx // optional
     // };
-// console.log(node.shepherd._rspsToResolve.mnode_1.read[0]);
-// console.log(node.shepherd._rspsToResolve.mnode_1.read[1]);
-// console.log(node.shepherd._rspsToResolve.mnode_1.read[2]);
+
     setTimeout(function () {
 //     setInterval(function () {
-            node.writeAttrsReq('/device/0/manuf', {
-                pmin: 20,
-                pmax: 100,
+            node.writeAttrsReq('/tempSensor/0/sensorValue', {
+                pmin: 5,
+                pmax: 15,
                 // gt: 1000,
                 // lt: 50,
                 //step: 200
             }).done(function (r) {
+
+                node.observeReq('/tempSensor/0/sensorValue').done();
+
              console.log('>>>>>>>> Write Attrs');
              console.log(r);
                 setTimeout(function () {
@@ -76,16 +78,197 @@ shepherd.on('registered', function (node) {
 
     }, 5000);
 
-    // node.readReq('tempSensor/').done(function (r) {
-    //     console.log('>>>>>>>> read tempSensor Object');
-    //     console.log(r);
-    // }, function (err) {
-    //     console.log(err);
-    // });
 
     // setTimeout(function () {
+    //     node.writeReq('/device/0/manuf', 'abc').done(function (r) {
+    //         console.log('>>>>>>>> writeReq /device/0/manuf');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5600);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/', { pmax: 333 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq Root');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5500);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/device', { pmax: 333 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5600);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/device1', { pmax: 333 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device1');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5700);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/device/0', { pmax: 321, pmin: 1 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device 0 ');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5800);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/device/1', { pmax: 321, pmin: 1 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device 1 ');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5900);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/device/0/pwrSrcVoltage', { lt: 500, gt: 1000, step: 60 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device 0 pwrSrcVoltage ');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6000);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/tempSensor/1x/sensorValue', { pmin: 20, gt: 300 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device 1 sensorValue ');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6000);
+
+    // setTimeout(function () {
+    //     node.writeAttrsReq('/tempSensor/1/sensorValue', { pminx: 20, gt: 300 }).done(function (r) {
+    //         console.log('>>>>>>>> writeReq device 1 sensorValue ');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6000);
+    // setTimeout(function () {
+    //     node.executeReq('/tempSensor/1/resetMinMaxMeaValues', [ 'param1', 'abc', 22 ]).done(function (r) {
+    //         console.log('>>>>>>>> executeReq resetMinMaxMeaValues');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5500);
+
+    // setTimeout(function () {
+    //     node.discoverReq('/').done(function (r) {
+    //         console.log('>>>>>>>> discover Root');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5500);
+
+    // setTimeout(function () {
+    //     node.discoverReq('/tempSensorx').done(function (r) {
+    //         console.log('>>>>>>>> discover Device');
+    //         console.log(util.inspect(r, { showHidden: true, depth: null }));
+    //         // console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6000);
+
+    // setTimeout(function () {
+    //     node.discoverReq('/device/0x').done(function (r) {
+    //         console.log('>>>>>>>> discover Device instance 0');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6500);
+
+    // setTimeout(function () {
+    //     node.discoverReq('/device/0/pwrSrcVoltagex').done(function (r) {
+    //         console.log('>>>>>>>> discover Device pwrSrcVoltage');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 7000);
+
+    // setTimeout(function () {
+    //     node.discoverReq('/3/0/0x').done(function (r) {
+    //         console.log('>>>>>>>> discover 3/0/0');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 7500);
+    // setTimeout(function () {
+    //     node.readReq('/').done(function (r) {
+    //         console.log('>>>>>>>> read tempSensor Object');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 5500);
+
+    // setTimeout(function () {
+    //     node.readReq('tempSensor/0').done(function (r) {
+    //         console.log('>>>>>>>> read tempSensor Instance 0');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6000);
+
+    // setTimeout(function () {
+    //     node.readReq('tempSensor/1').done(function (r) {
+    //         console.log('>>>>>>>> read tempSensor Instance 1');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 6500);
+
+    // setTimeout(function () {
+    //     node.readReq('tempSensor/1/sensorValue').done(function (r) {
+    //         console.log('>>>>>>>> read tempSensor resource 0/sensorValue');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 7000);
+
+    // setTimeout(function () {
+    //     node.readReq('tempSensor/1/maxRangeValue').done(function (r) {
+    //         console.log('>>>>>>>> read tempSensor resource 0/maxRangeValue');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 7500);
+
+    // setTimeout(function () {
+    //     node.readReq('tempSensor/1/resetMinMaxMeaValues').done(function (r) {
+    //         console.log('>>>>>>>> read tempSensor resource 1/resetMinMaxMeaValues');
+    //         console.log(r);
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }, 8000);
+    // setTimeout(function () {
     //     setInterval(function () {
-    //         node.readReq('tempSensor/0/').done(function (r) {
+    //         node.readReq('tempSensor/1/').done(function (r) {
     //             console.log('>>>>>>>> read tempSensor Instance');
     //             console.log(r);
     //         }, function (err) {
--- test.js
+++ test.js
@@ -1,11 +1,2 @@
-MDEFS = require('./lib/defs/mdefs');
-
-// console.log(JSON.stringify({x:3}));
-// console.log(typeof JSON.parse('3'));
-
-// console.log(typeof MDEFS.getOidNumber('3'));
-//console.log(parseInt('60'));
-
-var str = "E"; 
-var res = str.match(/R/g);
-console.log(res);
+var points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort());
