3f62507a94f778d69d9538f2af6cb2972aadb0d7
Simen Li <simenkid@gmail.com>
2016-02-04 18:52:04 +0800

    avoid using ES6 string template, since node version on MT7866 does not support ES6

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
+var _ = require('lodash');
 var points = [40, 100, 1, 5, 25, 10];
-console.log(points.sort());
+//console.log(points.sort());
+
+function x() {
+	var c = arguments[0],
+		path = '';
+	_.forEach(arguments, function (arg, i) {
+		if (i === 0) return;
+		path = path + arg + c;
+	});
+
+	return path.slice(0, path.length - 1);
+}
+
+console.log(x('/', 'xsd', 1, 2, 'hh', 6, 7, 111));
